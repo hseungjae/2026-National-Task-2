e8b9696f43fcbd766ba07f6f9413e70dde43d876
한승재 <hanseungjae0812@gmail.com>
2026-06-30 14:34:50 +0900

    Update authenticationMode from ConfigMap to API

--- 010/1. EKS Scaling/eks/cluster.tf
+++ 010/1. EKS Scaling/eks/cluster.tf
@@ -11,7 +11,7 @@ module "eks" {
   endpoint_public_access  = true
   endpoint_private_access = true
 
-  authentication_mode                      = "API_AND_CONFIG_MAP"
+  authentication_mode                      = "API"
   enable_cluster_creator_admin_permissions = true
 
   enable_irsa = true
